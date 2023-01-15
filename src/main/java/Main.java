558eff9dadfecbdb457e16e39199999221805e82
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-01-15 20:05:25 +0300

    1. Boolean operators 2. Mathematical operators 3. Overflows in calculation 4. Combinations of data types (int and double)

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+    int a = 34;
+    int b = 53;
+    byte c = 125;
+    double d = 1.5;
+
+    // Mathematical operators
+    int result_1 = a + b - 1 * a / 2;
+        System.out.println("Mathematical operators: " +result_1);
+    // Boolean operators
+        if (a != 0 && b < 1000) {
+            System.out.println("Boolean operators");
+        }
+    //Overflows in calculations of the Byte type
+        int result_3 = c * 5;
+        System.out.println("Overflows in calculations of the Byte type: " + result_3);
+    //Combinations of data types (int and double)
+      double result_5 = a + d + (b / 2);
+        System.out.println( "Combinations of data types (int and double): " + result_5);
+}
+}
